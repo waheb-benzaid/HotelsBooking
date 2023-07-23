fccc802f419a50ba8213f09e4c4367b3882bcb21
waheb-benzaid <waheb.benzaid@gmail.com>
2023-07-23 18:16:07 +0100

    implementation of hotel controller (CRUD)

---
+++ BookingHotels.Api/Controllers/HotelController.cs
@@ -0,0 +1,89 @@
+﻿using BookingHotels.Domain.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookingHotels.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HotelController : ControllerBase
+    {
+        public HotelController()
+        {
+
+        }
+
+        [HttpGet]
+        public IActionResult GetAllHotels()
+        {
+            var hotels = GetHotels();
+            return Ok(hotels);
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult GetHotelById(int id)
+        {
+            var hotels = GetHotels();
+            var hotel = hotels.FirstOrDefault(h => h.HotelId == id);
+            if (hotel == null)
+            {
+                return NotFound();
+            }
+            return Ok(hotel);
+        }
+
+        [HttpPost]
+        public IActionResult CreateHotel([FromBody] Hotel hotel)
+        {
+            var hotels = GetHotels();
+            hotels.Add(hotel);
+            return Created("", hotel);
+        }
+
+        [Route("id")]
+        [HttpPut]
+        public IActionResult UpdateHotel([FromBody] Hotel updatedHotel, int id)
+        {
+            var hotels = GetHotels();
+            var oldHotel = hotels.FirstOrDefault(h => h.HotelId == id);
+            hotels.Remove(oldHotel);
+            hotels.Add(updatedHotel);
+            return NoContent();
+        }
+
+        [Route("id")]
+        [HttpDelete]
+        public IActionResult DeleteHotelById(int id)
+        {
+            var hotels = GetHotels();
+            var hotelToDelete = hotels.FirstOrDefault(h => h.HotelId == id);
+            if (hotelToDelete == null)  return NotFound();
+            hotels.Remove(hotelToDelete);
+            return NoContent();
+        }
+
+        private List<Hotel> GetHotels() // just for test before getting started with the database
+        {
+            return new List<Hotel>
+            {
+                new Hotel
+                {
+                    HotelId= 1,
+                    Name="Sabri",
+                    Stars = 4,
+                    Country= "Algeria",
+                    City="Annaba"
+                },
+                new Hotel
+                {
+                    HotelId= 2,
+                    Name="Sheraton",
+                    Stars=5,
+                    Country="Algeria",
+                    City="Annaba"
+                }
+            };
+        }
+    }
+}
--- BookingHotels.Domain/Models/Hotel.cs
+++ BookingHotels.Domain/Models/Hotel.cs
@@ -11,12 +11,11 @@ namespace BookingHotels.Domain.Models
         public int HotelId { get; set; }
         public string Name { get; set; }
         public int Stars { get; set; }
-
         public string Address { get; set; }
         public string City { get; set; }
         public string Country { get; set; }
-        public string Phone { get; set; }
-
-        public List<Room> Rooms { get; set; }
+       // public string Phone { get; set; }
+       // public List<Room> Rooms { get; set; }
+        public string Description { get; set; }
     }
 }
