bf6dc42a39ff2cfd0a1a76b25da4ef416290b2cb
waheb-benzaid <waheb.benzaid@gmail.com>
2023-07-24 23:40:38 +0100

    add needed controllers

---
+++ BookingHotels.Api/Controllers/RoomController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookingHotels.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoomController : ControllerBase
+    {
+    }
+}
